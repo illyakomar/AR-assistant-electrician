1291d377a17760ec522350df366094a80f7a8ebb
Illya <illya-komar@ukr.net>
2021-02-21 11:58:05 +0200

    Add Lean

---
+++ AR Assistant Electrician/Assets/Scripts/UIManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+
+    public GameObject loginUI;
+    public GameObject registerUI;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            Debug.Log("Instance already exists, destroying object!");
+            Destroy(this);
+        }
+    }
+
+    public void LoginScreen()
+    {
+        loginUI.SetActive(true);
+        registerUI.SetActive(false);
+    }
+    public void RegisterScreen()
+    {
+        loginUI.SetActive(false);
+        registerUI.SetActive(true);
+    }
+}
