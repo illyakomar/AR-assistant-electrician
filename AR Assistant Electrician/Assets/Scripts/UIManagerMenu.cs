6eed693b315e4712bf1d940a6dc994c0b8b122fa
Illya <illya-komar@ukr.net>
2021-03-04 12:12:26 +0200

    Add menu v1.1

---
+++ AR Assistant Electrician/Assets/Scripts/UIManagerMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManagerMenu : MonoBehaviour
+{
+    public static UIManagerMenu instance;
+
+    public GameObject menuUI;
+    public GameObject menuLabUI;
+    public GameObject labUI;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            Debug.Log("Instance already exists, destroying object!");
+            Destroy(this);
+        }
+    }
+
+    public void MenuScreen()
+    {
+        menuUI.SetActive(true);
+        menuLabUI.SetActive(false);
+    }
+
+    public void MenuLabUIScreen()
+    {
+        menuUI.SetActive(false);
+        menuLabUI.SetActive(true);
+    }
+
+    public void LabUIScreen()
+    {
+        menuLabUI.SetActive(false);
+        labUI.SetActive(true);
+    }
+}
