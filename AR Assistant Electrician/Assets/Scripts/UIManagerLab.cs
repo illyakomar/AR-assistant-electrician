1cbefc4f359c25b8a7cd9201b2ed45d524f65936
Illya <illya-komar@ukr.net>
2021-03-09 10:31:26 +0200

    Add lab1 v1

---
+++ AR Assistant Electrician/Assets/Scripts/UIManagerLab.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManagerLab : MonoBehaviour
+{
+    public static UIManagerLab instance;
+
+    public GameObject LabTask;
+    public GameObject LabLabEquipment;
+    public GameObject AllLab;
+    public GameObject Ammert;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            Debug.Log("Instance already exists, destroying object!");
+            Destroy(this);
+        }
+    }
+
+    public void LabEquipment()
+    {
+        LabLabEquipment.SetActive(true);
+        LabTask.SetActive(false);
+    }
+
+    public void Ammeter()
+    {
+        Ammert.SetActive(true);
+        LabLabEquipment.SetActive(false);
+        AllLab.SetActive(false);
+    }
+
+   
+}
