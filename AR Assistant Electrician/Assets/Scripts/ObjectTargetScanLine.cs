7b428a78a0482e8f1be98b9cda177486485b61ff
Illya <illya-komar@ukr.net>
2021-03-17 15:58:39 +0200

    Add lab 1 v1.2

---
+++ AR Assistant Electrician/Assets/Scripts/ObjectTargetScanLine.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class ObjectTargetScanLine : MonoBehaviour
+{
+    const float SCAN_DURATION = 4; //seconds
+    float mTime;
+    bool mMovingDown = true;
+    Camera m_Camera;
+ 
+    void Start()
+    {
+        m_Camera = Camera.main;
+    }
+
+    void Update()
+    {
+        float u = mTime / SCAN_DURATION;
+        mTime += Time.deltaTime;
+        if (u > 1)
+        {
+            
+            mMovingDown = !mMovingDown;
+            u = 0;
+            mTime = 0;
+        }
+
+        
+        float viewAspect = m_Camera.pixelWidth / (float)m_Camera.pixelHeight;
+        float fovY = Mathf.Deg2Rad * m_Camera.fieldOfView;
+        float depth = 1.02f * m_Camera.nearClipPlane;
+        float viewHeight = 2 * depth * Mathf.Tan(0.5f * fovY);
+        float viewWidth = viewHeight * viewAspect;
+
+        
+        float y = -0.5f * viewHeight + u * viewHeight;
+        if (mMovingDown)
+        {
+            y *= -1;
+        }
+
+        transform.localPosition = new Vector3(0, y, depth);
+
+        
+        float scaleX = 1.02f * viewWidth;
+        float scaleY = scaleX / 32;
+        transform.localScale = new Vector3(scaleX, scaleY, 1.0f);
+    }
+}
--- AR Assistant Electrician/Assets/Scripts/UIManagerLab.cs
+++ AR Assistant Electrician/Assets/Scripts/UIManagerLab.cs
@@ -9,7 +9,11 @@ public class UIManagerLab : MonoBehaviour
     public GameObject LabTask;
     public GameObject LabLabEquipment;
     public GameObject AllLab;
-    public GameObject Ammert;
+    public GameObject Ammeter;
+    public GameObject Voltmeter;
+    public GameObject SupportShop;
+    public GameObject Source;
+    public GameObject Camera;
 
     private void Awake()
     {
@@ -30,12 +34,73 @@ public class UIManagerLab : MonoBehaviour
         LabTask.SetActive(false);
     }
 
-    public void Ammeter()
+    public void LabTaskBack()
     {
-        Ammert.SetActive(true);
+        LabTask.SetActive(true);
+        LabLabEquipment.SetActive(false);
+    }
+
+    public void OnAmmeter()
+    {
+        Camera.SetActive(true);
+        Ammeter.SetActive(true);
         LabLabEquipment.SetActive(false);
         AllLab.SetActive(false);
     }
 
-   
+    public void OnVoltmeter()
+    {
+        Camera.SetActive(true);
+        Voltmeter.SetActive(true);
+        LabLabEquipment.SetActive(false);
+        AllLab.SetActive(false);
+    }
+
+    public void OnSupportShop()
+    {
+        Camera.SetActive(true);
+        SupportShop.SetActive(true);
+        LabLabEquipment.SetActive(false);
+        AllLab.SetActive(false);
+    }
+
+    public void OnSource()
+    {
+        Source.SetActive(true);
+        Camera.SetActive(true);
+        LabLabEquipment.SetActive(false);
+        AllLab.SetActive(false);
+    }
+
+    public void FromAmmeter()
+    {
+        Ammeter.SetActive(false);
+        Camera.SetActive(false);
+        LabLabEquipment.SetActive(true);
+        AllLab.SetActive(true);
+    }
+
+    public void FromVoltmeter()
+    {
+        Voltmeter.SetActive(false);
+        Camera.SetActive(false);
+        LabLabEquipment.SetActive(true);
+        AllLab.SetActive(true);
+    }
+
+    public void FromSupportShop()
+    {
+        SupportShop.SetActive(false);
+        Camera.SetActive(false);
+        LabLabEquipment.SetActive(true);
+        AllLab.SetActive(true);
+    }
+
+    public void FromSource()
+    {
+        Source.SetActive(false);
+        Camera.SetActive(false);
+        LabLabEquipment.SetActive(true);
+        AllLab.SetActive(true);
+    }
 }
