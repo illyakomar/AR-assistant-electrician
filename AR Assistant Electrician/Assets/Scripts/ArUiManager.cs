914c08a872b2d85607688fd745831f502ba27a85
Illya <illya-komar@ukr.net>
2021-04-05 18:19:08 +0300

    Add lab 1 v1.5

---
+++ AR Assistant Electrician/Assets/Scripts/ArUiManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArUiManager : MonoBehaviour
+{
+    public static ArUiManager instance;
+
+    public GameObject AmmeterInfo;
+    public GameObject AmmeterYouTube;
+    public GameObject AmmeterLink;
+    public bool info;
+    public bool youtube;
+    public bool link;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            Debug.Log("Instance already exists, destroying object!");
+            Destroy(this);
+        }
+    }
+
+    public void Info()
+    {
+        if (!info)
+        {
+            AmmeterInfo.SetActive(true);
+            info = true;
+        }
+        else
+        {
+            AmmeterInfo.SetActive(false);
+            info = false;
+        }
+    }
+
+    public void YouTube()
+    {
+        if (!youtube)
+        {
+            AmmeterYouTube.SetActive(true);
+            youtube = true;
+        }
+        else
+        {
+            AmmeterYouTube.SetActive(false);
+            youtube = false;
+        }
+    }
+
+    public void Link()
+    {
+        if (!link)
+        {
+            AmmeterLink.SetActive(true);
+            link = true;
+        }
+        else
+        {
+            AmmeterLink.SetActive(false);
+            link = false;
+        }
+    }
+
+    public void Wikipedia()
+    {
+        Application.OpenURL("https://uk.wikipedia.org/wiki/Амперметр");
+    }
+
+    public void Portal()
+    {
+        Application.OpenURL("https://uk.mosg-portal.com/an-ammeter-work-4963680-463");
+    }
+}
---
+++ AR Assistant Electrician/Assets/Scripts/VideoController.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+public class VideoController : MonoBehaviour
+{
+    private VideoPlayer videoPlayer;
+    public Button m_PlayButton;
+    public RectTransform m_ProgressBar;
+
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+
+        videoPlayer.errorReceived += HandleVideoError;
+        videoPlayer.started += HandleStartedEvent;
+        videoPlayer.prepareCompleted += HandlePrepareCompleted;
+        videoPlayer.seekCompleted += HandleSeekCompleted;
+        videoPlayer.loopPointReached += HandleLoopPointReached;
+
+        LogClipInfo();
+    }
+
+    void Update()
+    {
+
+        if (videoPlayer.isPlaying)
+        {
+            ShowPlayButton(false);
+
+            if (videoPlayer.frameCount < float.MaxValue)
+            {
+                float frame = (float)videoPlayer.frame;
+                float count = (float)videoPlayer.frameCount;
+
+                float progressPercentage = 0;
+
+                if (count > 0)
+                    progressPercentage = (frame / count) * 100.0f;
+
+                if (m_ProgressBar != null)
+                    m_ProgressBar.sizeDelta = new Vector2((float)progressPercentage, m_ProgressBar.sizeDelta.y);
+            }
+
+        }
+        else
+        {
+            ShowPlayButton(true);
+        }
+    }
+
+    void OnApplicationPause(bool pause)
+    {
+        Debug.Log("OnApplicationPause(" + pause + ") called.");
+        if (pause)
+            Pause();
+    }
+
+ 
+
+    public void Play()
+    {
+        Debug.Log("Play Video");
+        PauseAudio(false);
+        videoPlayer.Play();
+        ShowPlayButton(false);
+    }
+
+    public void Pause()
+    {
+        if (videoPlayer)
+        {
+            Debug.Log("Pause Video");
+            PauseAudio(true);
+            videoPlayer.Pause();
+            ShowPlayButton(true);
+        }
+    }
+
+    private void PauseAudio(bool pause)
+    {
+        for (ushort trackNumber = 0; trackNumber < videoPlayer.audioTrackCount; ++trackNumber)
+        {
+            if (pause)
+                videoPlayer.GetTargetAudioSource(trackNumber).Pause();
+            else
+                videoPlayer.GetTargetAudioSource(trackNumber).UnPause();
+        }
+    }
+
+    private void ShowPlayButton(bool enable)
+    {
+        m_PlayButton.enabled = enable;
+        m_PlayButton.GetComponent<Image>().enabled = enable;
+    }
+
+    private void LogClipInfo()
+    {
+        if (videoPlayer.clip != null)
+        {
+            string stats =
+                "\nName: " + videoPlayer.clip.name +
+                "\nAudioTracks: " + videoPlayer.clip.audioTrackCount +
+                "\nFrames: " + videoPlayer.clip.frameCount +
+                "\nFPS: " + videoPlayer.clip.frameRate +
+                "\nHeight: " + videoPlayer.clip.height +
+                "\nWidth: " + videoPlayer.clip.width +
+                "\nLength: " + videoPlayer.clip.length +
+                "\nPath: " + videoPlayer.clip.originalPath;
+
+            Debug.Log(stats);
+        }
+    }
+
+    void HandleVideoError(VideoPlayer video, string errorMsg)
+    {
+        Debug.LogError("Error: " + video.clip.name + "\nError Message: " + errorMsg);
+    }
+
+    void HandleStartedEvent(VideoPlayer video)
+    {
+        Debug.Log("Started: " + video.clip.name);
+    }
+
+    void HandlePrepareCompleted(VideoPlayer video)
+    {
+        Debug.Log("Prepare Completed: " + video.clip.name);
+    }
+
+    void HandleSeekCompleted(VideoPlayer video)
+    {
+        Debug.Log("Seek Completed: " + video.clip.name);
+    }
+
+    void HandleLoopPointReached(VideoPlayer video)
+    {
+        Debug.Log("Loop Point Reached: " + video.clip.name);
+
+        ShowPlayButton(true);
+    }
+
+}
