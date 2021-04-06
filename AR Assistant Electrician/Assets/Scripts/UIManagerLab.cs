9bf05e076078aa9395a06cee7ee325bee3e94dcc
Illya <illya-komar@ukr.net>
2021-04-06 18:26:11 +0300

    Fix lab 1

--- AR Assistant Electrician/Assets/Scripts/UIManagerLab.cs
+++ AR Assistant Electrician/Assets/Scripts/UIManagerLab.cs
@@ -15,6 +15,19 @@ public class UIManagerLab : MonoBehaviour
     public GameObject Source;
     public GameObject Camera;
 
+    public GameObject AmmeterInfo;
+    public GameObject AmmeterYouTube;
+    public GameObject AmmeterLink;
+    public GameObject VoltmeterInfo;
+    public GameObject VoltmeterYouTube;
+    public GameObject VoltmeterLink;
+    public GameObject SupportShopInfo;
+    public GameObject SupportShopYouTube;
+    public GameObject SupportShopLink;
+    public bool info;
+    public bool youtube;
+    public bool link;
+
     private void Awake()
     {
         if (instance == null)
@@ -78,6 +91,9 @@ public class UIManagerLab : MonoBehaviour
         Camera.SetActive(false);
         LabLabEquipment.SetActive(true);
         AllLab.SetActive(true);
+        info = false;
+        youtube = false;
+        link = false;
     }
 
     public void FromVoltmeter()
@@ -86,6 +102,9 @@ public class UIManagerLab : MonoBehaviour
         Camera.SetActive(false);
         LabLabEquipment.SetActive(true);
         AllLab.SetActive(true);
+        info = false;
+        youtube = false;
+        link = false;
     }
 
     public void FromSupportShop()
@@ -94,6 +113,9 @@ public class UIManagerLab : MonoBehaviour
         Camera.SetActive(false);
         LabLabEquipment.SetActive(true);
         AllLab.SetActive(true);
+        info = false;
+        youtube = false;
+        link = false;
     }
 
     public void FromSource()
@@ -102,5 +124,161 @@ public class UIManagerLab : MonoBehaviour
         Camera.SetActive(false);
         LabLabEquipment.SetActive(true);
         AllLab.SetActive(true);
+    }
+
+    public void InfoAmmeter()
+    {
+        if (!info)
+        {
+            AmmeterInfo.SetActive(true);
+            info = true;
+        }
+        else
+        {
+            AmmeterInfo.SetActive(false);
+            info = false;
+        }
+    }
+
+    public void InfoVoltmeter()
+    {
+        if (!info)
+        {
+            VoltmeterInfo.SetActive(true);
+            info = true;
+        }
+        else
+        {
+            VoltmeterInfo.SetActive(false);
+            info = false;
+        }
+    }
+
+    public void InfoSupportShop()
+    {
+        if (!info)
+        {
+            SupportShopInfo.SetActive(true);
+            info = true;
+        }
+        else
+        {
+            SupportShopInfo.SetActive(false);
+            info = false;
+        }
+    }
+
+    public void YouTubeAmmeter()
+    {
+        if (!youtube)
+        {
+            AmmeterYouTube.SetActive(true);
+            youtube = true;
+        }
+        else
+        {
+            AmmeterYouTube.SetActive(false);
+            youtube = false;
+        }
+    }
+
+    public void YouTubeVoltmeter()
+    {
+        if (!youtube)
+        {
+            VoltmeterYouTube.SetActive(true);
+            youtube = true;
+        }
+        else
+        {
+            VoltmeterYouTube.SetActive(false);
+            youtube = false;
+        }
+    }
+
+    public void YouTubeSupportShop()
+    {
+        if (!youtube)
+        {
+            SupportShopYouTube.SetActive(true);
+            youtube = true;
+        }
+        else
+        {
+            SupportShopYouTube.SetActive(false);
+            youtube = false;
+        }
+    }
+
+    public void LinkAmmeter()
+    {
+        if (!link)
+        {
+            AmmeterLink.SetActive(true);
+            link = true;
+        }
+        else
+        {
+            AmmeterLink.SetActive(false);
+            link = false;
+        }
+    }
+
+    public void LinkVoltmeter()
+    {
+        if (!link)
+        {
+            VoltmeterLink.SetActive(true);
+            link = true;
+        }
+        else
+        {
+            VoltmeterLink.SetActive(false);
+            link = false;
+        }
+    }
+
+    public void LinkSupportShop()
+    {
+        if (!link)
+        {
+            SupportShopLink.SetActive(true);
+            link = true;
+        }
+        else
+        {
+            SupportShopLink.SetActive(false);
+            link = false;
+        }
+    }
+
+    public void WikipediaAmmeter()
+    {
+        Application.OpenURL("https://uk.wikipedia.org/wiki/Амперметр");
+    }
+
+    public void PortalAmmeter()
+    {
+        Application.OpenURL("https://uk.mosg-portal.com/an-ammeter-work-4963680-463");
+    }
+
+    public void WikipediaVoltmeter()
+    {
+        Application.OpenURL("https://uk.wikipedia.org/wiki/Вольтметр");
+    }
+
+    public void PortalVoltmeter()
+    {
+        Application.OpenURL("https://uk.845audio.org/Menggunakan-Voltmeter-5638");
+    }
+
+    public void WikipediaSupportShop()
+    {
+        Application.OpenURL("https://ru.wikipedia.org/wiki/Мера_электрического_сопротивления");
+    }
+
+    public void PortalSupportShop()
+    {
+        Application.OpenURL("http://standart-m.com.ua/elektroizmeritelnoe-oborudovanie/magaziny-soprotivleniy/magaziny-soprotivleniy-r33?mova=uk");
     }
 }
