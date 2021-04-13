6e26f5a8cc7e98c8f3ba6e9603d50073ddc4c732
Illya <illya-komar@ukr.net>
2021-04-13 12:59:25 +0300

    Add lab 1 v1.9

---
+++ AR Assistant Electrician/Assets/Scripts/ProrgressManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProrgressManager : MonoBehaviour
+{
+    public static ProrgressManager instance;
+    public GameObject StartLab;
+    public GameObject Screen1;
+    public GameObject Screen2;
+    public GameObject Screen3;
+    public GameObject Screen4;
+    public GameObject Step1;
+    public GameObject Step2;
+    public GameObject Step3;
+    public GameObject Step4;
+    public GameObject SupportShop;
+    public GameObject Camera;
+    public GameObject Green;
+    public GameObject ScanLine;
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            Debug.Log("Instance already exists, destroying object!");
+            Destroy(this);
+        }
+    }
+
+    public void LabStart()
+    {
+        StartLab.SetActive(false);
+        Camera.SetActive(false);
+        Screen1.SetActive(true);
+    }
+
+    public void LoadScreen2()
+    {
+        Screen1.SetActive(false);
+        Screen2.SetActive(true);
+        Step1.SetActive(false);
+        Step2.SetActive(true);
+    }
+
+    public void LoadScreen3()
+    {
+        Screen2.SetActive(false);
+        Screen3.SetActive(true);
+        Step2.SetActive(false);
+        Step3.SetActive(true);
+    }
+
+    public void LoadScreen4()
+    {
+        Screen3.SetActive(false);
+        Screen4.SetActive(true);
+        Step3.SetActive(false);
+        Step4.SetActive(true);
+    }
+
+    public void FromScreen2()
+    {
+        Screen1.SetActive(true);
+        Screen2.SetActive(false);
+        Step1.SetActive(true);
+        Step2.SetActive(false);
+    }
+
+    public void FromScreen3()
+    {
+        Screen2.SetActive(true);
+        Screen3.SetActive(false);
+        Step2.SetActive(true);
+        Step3.SetActive(false);
+    }
+}
