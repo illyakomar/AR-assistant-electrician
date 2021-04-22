f24bcb8e820ffdb7ff6358ac18f59c0b1ba8eb14
Illya <illya-komar@ukr.net>
2021-04-22 16:03:34 +0300

    Add photon v1.1

---
+++ AR Assistant Electrician/Assets/Scripts/PickupObject.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupObject : MonoBehaviour {
+
+	public GameObject PickPotionButton;
+	public GameObject DropPotionButton;
+	public float distance;
+	public float smooth;
+
+	bool carrying;
+	GameObject mainCamera;
+	GameObject carriedObject;
+
+	void Start () {
+		PickPotionButton.SetActive (true);
+		DropPotionButton.SetActive (false);
+		mainCamera = GameObject.FindWithTag("MainCamera");
+	}
+	
+	void Update () {
+		if(carrying) {
+			carry(carriedObject);
+
+		} else {
+			pickObject();
+		}
+	}
+	
+	void rotateObject() {
+		carriedObject.transform.Rotate(5,10,15);
+	}
+	
+	void carry(GameObject o) {
+		o.transform.position = Vector3.Lerp (o.transform.position, mainCamera.transform.position + mainCamera.transform.forward * distance, Time.deltaTime * smooth);
+	}
+
+	public void pickObject() {
+		int x = Screen.width / 2;
+		int y = Screen.height / 2;
+
+		Ray ray = mainCamera.GetComponent<Camera>().ScreenPointToRay(new Vector3(x,y));
+		RaycastHit hit;
+		if(Physics.Raycast(ray, out hit)) {
+			Pickupable p = hit.collider.GetComponent<Pickupable>();
+			if(p != null) {
+				carrying = true;
+				carriedObject = p.gameObject;
+				p.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+				PickPotionButton.SetActive (false);
+				DropPotionButton.SetActive (true);
+
+			}
+		}
+	}
+	
+	void checkDrop() {
+		if(Input.GetMouseButtonDown(1)) {
+			dropObject();
+		}
+	}
+	
+	public void dropObject() {
+		carrying = false;
+		if(carriedObject != null)
+			carriedObject.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+		carriedObject = null;
+		PickPotionButton.SetActive (true);
+		DropPotionButton.SetActive (false);
+	}
+}
+
