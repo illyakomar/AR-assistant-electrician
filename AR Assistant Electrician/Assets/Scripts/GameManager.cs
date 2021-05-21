357a45756930dce2ec5a579336203578eba1ff39
Illya <illya-komar@ukr.net>
2021-05-21 10:10:09 +0300

    Add photon v2.2

--- AR Assistant Electrician/Assets/Scripts/GameManager.cs
+++ AR Assistant Electrician/Assets/Scripts/GameManager.cs
@@ -4,9 +4,32 @@ using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 using System.Linq;
+using TMPro;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviourPunCallbacks
+{ 
+    public TextMeshProUGUI playerListText;
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        UpdateLobbyUI();
+    }
+
+    [PunRPC]
+    public void UpdateLobbyUI()
+    {
+        playerListText.text = "";
+
+        foreach (Player player in PhotonNetwork.PlayerList)
+        {
+            if (player.IsMasterClient)
+                playerListText.text += player.NickName + " (Host) \n";
+            else
+                playerListText.text += player.NickName + "\n";
+        }
+
+    }
+
     public void GoBackToMenu()
     {
         PhotonNetwork.LeaveRoom();
---
+++ AR Assistant Electrician/Assets/Scripts/VirtualButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VirtualButton : MonoBehaviour
+{
+    public GameObject vbBtnObj;
+    public GameObject cubeAni;
+
+    // Use this for initialization
+    void Start()
+    {
+        vbBtnObj = GameObject.Find("AmmeterBtn");
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(OnButtonPressed);
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(OnButtonReleased);
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        cubeAni.SetActive(true);
+        Debug.Log("Button pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        cubeAni.SetActive(false);
+        Debug.Log("Button released");
+    }
+}
